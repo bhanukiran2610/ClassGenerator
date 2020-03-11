124e181f4cfbb361043a4e2780af76e8bbd3a039
Bhanu <Bhanu@Bhanu>
2020-03-11 14:54:16 +0530

    commit

---
+++ src/fi/generator/Field.java
@@ -0,0 +1,8 @@
+package fi.generator;
+
+public class Field {
+	String fieldAccessSpecifier;
+	String fieldType;
+	String fieldName;
+	
+}
---
+++ src/fi/generator/Method.java
@@ -0,0 +1,7 @@
+package fi.generator;
+
+public class Method {
+	String methodAccessSpecifier;
+	String methodName;
+	String methodReturnType;
+}
